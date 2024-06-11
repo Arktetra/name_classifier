fc1d6f596ac83465701e5fceb897409ca5e32603
Arktetra <sameershrestha.sm@gmail.com>
2024-06-11 19:35:46 +0545

    add: pre-commit hook to git

--- name_classifier/run_experiments.py
+++ name_classifier/run_experiments.py
@@ -1,3 +1,5 @@
+"""A module for running experiments."""
+
 from name_classifier.models.rnn import RNN
 from name_classifier.data.data_setup import create_dataloaders
 from name_classifier.data.utils import custom_collate_function
@@ -12,6 +14,7 @@ from pathlib import Path
 import torch
 
 def main():
+    """The main function."""
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     train_dataloader, test_dataloader = create_dataloaders(
@@ -29,19 +32,19 @@ def main():
         model = RNN(metadata.N_LETTERS, 128, metadata.N_CATEGORIES)
         model_name = "Simple_RNN"
         model.to(device)
-        
+
         print(f"[INFO] model name: {model_name}")
         print(f"[INFO] epochs: {epochs}")
-        
-        
-        
+
+
+
         criterion = torch.nn.NLLLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr = 0.005)
         writer = create_writer(
             model_name,
             str(epochs)
         )
-        
+
         train(
             model = model,
             train_dataloader = train_dataloader,
@@ -52,16 +55,16 @@ def main():
             device = device,
             writer = writer
         )
-        
+
         file_path = f"simple_rnn_{epochs}_epochs.pth"
-        
+
         save_model(
             model = model,
             dir = "models",
             model_name = file_path
         )
-        
+
         print("-" * 100 + "\n")
-    
+
 if __name__ == "__main__":
     main()
